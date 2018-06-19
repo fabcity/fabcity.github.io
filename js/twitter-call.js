3273c0b741f2048e96aad5d9b97437f276105681
m <m.quintero09@gmail.com>
2018-06-19 17:48:12 +0200

    twitter box v0 stable

--- js/twitter-call.js
+++ js/twitter-call.js
@@ -1,4 +1,4 @@
-$.getJSON("https://twitter.madeat.eu/1.1/statuses/user_timeline.json?screen_name=fabfndn&count=1", function(data) {
+$.getJSON("https://twitter.madeat.eu/1.1/statuses/user_timeline.json?screen_name=fabcityglobal&count=1", function(data) {
   var tweet = linkify(data[0].text);
   $('.tweet').html(tweet);
 });
