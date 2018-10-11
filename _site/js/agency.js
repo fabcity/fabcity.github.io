8466b902f22984cec452c3861488d8cda8d91a1f
m <m.quintero09@gmail.com>
2018-10-11 11:44:27 +0200

    js cleanup

--- _site/js/agency.js
+++ _site/js/agency.js
@@ -40,3 +40,48 @@ $('.cities-left').click(function() {
   // If we click to Show any city, hide all cities already shown
   $('.city-info.collapse').removeClass('in');
 });
+
+/*Mobile responsiveness for section height*/
+$(window).load(function() {
+    var viewportWidth = $(window).width();
+    if (viewportWidth < 767) {
+        $(".section-texts").css("height", "auto");
+        $(".section-img-box").css("height", "auto");
+    }
+ });
+$(window).resize(function () {
+    var viewportWidth = $(window).width();
+    if (viewportWidth < 767) {
+        $(".section-texts").css("height", "auto");
+        $(".section-img-box").css("height", "auto");
+    }
+});
+
+/*Sharing popover on section buttons*/
+// default init
+//$(function () {
+//  $('[data-toggle="popover"]').popover()
+//})
+
+$('[data-toggle="popover"]').popover({
+  html : true, 
+  container : '#btn-share',
+  content: function() {
+    return $('#popoverExampleHiddenContent').html();
+  },
+  template: '<div class="popover" role="tooltip"><div class="popover-content"></div></div>'
+});
+
+$(document).click(function (event) {
+            // hide share button popover
+        if (!$(event.target).closest('#btn-share').length) {
+            $('#btn-share').popover('hide')
+        }
+    });
+
+/*
+$("a.facebook").attr("href", "https://www.facebook.com/sharer/sharer.php?u=" + window.location.href);
+*/
+
+/*
+$("a.google-plus").attr("href", "https://plus.google.com/share?url=" + window.location.href);*/
